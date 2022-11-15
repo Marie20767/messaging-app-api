7efd617990344844ebb29967af31d44e3082d0d6
Marie20767 <marie20767@gmail.com>
2022-11-15 16:45:11 +0000

    Added domain to cors to get socket.io to work on mobile devices

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'http://192.168.1.42:3000'],
     methods: ['GET', 'POST'],
   },
 });
